5baec8b767ff7563a2b95fe46623aafde64eee63
Victor Castaneda <v.castanedap@uniandes.edu.co>
2024-11-16 22:05:13 -0500

    Add cypress tests

--- cypress/e2e/profile.cy.ts
+++ cypress/e2e/profile.cy.ts
@@ -11,23 +11,27 @@ describe('Agent List Page', () => {
   });
 
   it('Should display the incidences list table with correct columns', () => {
-    cy.get('incidences-table').should('be.visible');
-    cy.get('th-code').contains('Codigo').should('exist');
-    cy.get('th-description').contains('Descripción').should('exist');
-    cy.get('th-subject').contains('Asunto').should('exist');
-    cy.get('th-created-at').contains('Creado en').should('exist');
-    cy.get('th-updated-at').contains('Actualizado en').should('exist');
-    cy.get('th').contains('Acciones').should('exist');
+    cy.get('#incidences-table').should('be.visible');
+    cy.get('#th-code').contains('Codigo').should('exist');
+    cy.get('#th-description').contains('Descripción').should('exist');
+    cy.get('#th-subject').contains('Asunto').should('exist');
+    cy.get('#th-created-at').contains('Creado en').should('exist');
+    cy.get('#th-updated-at').contains('Actualizado en').should('exist');
+    cy.get('#th').contains('Acciones').should('exist');
   });
 
-  it('Should navigate to create agent form and register a new agent', () => {
-    cy.get('#btn-create-agent').click();
-    cy.url().should('include', '/dashboard/register/agent');
+  it('Should display the agents list table with correct columns', () => {
+    cy.get('#incidences-table').should('be.visible');
+    cy.get('#th-user').contains('Codigo').should('exist');
+    cy.get('#th-identification-agent').contains('Descripción').should('exist');
+    cy.get('#th-fullname').contains('Asunto').should('exist');
+    cy.get('#th-email').contains('Creado en').should('exist');
+    cy.get('#th-phone').contains('Actualizado en').should('exist');
+    cy.get('#th-actions-agent').contains('Acciones').should('exist');
   });
 
-  it('Should navigate through pages using the paginator', () => {
-    cy.get('incidences-table').should('be.visible');
-    cy.get('mat-paginator').should('be.visible');
+  it('Should navigate to plans', () => {
+    cy.get('#btn-dashboard-plans').click();
+    cy.url().should('include', '/dashboard/plans');
   });
-
 });

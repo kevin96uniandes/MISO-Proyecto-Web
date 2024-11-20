1b743bc2530fedc6c9c21bf837f59baba47b41e3
Kevin Alexander Maldonado Delgado <k.maldonadod@uniandes.edu.co>
2024-11-20 15:56:28 -0500

    merge develop

--- src/app/pages/invoice/pipe/invoice-state.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { InvoiceStatePipe } from './invoice-state.pipe';
-
-describe('InvoiceStatePipe', () => {
-  it('create an instance', () => {
-    const pipe = new InvoiceStatePipe();
-    expect(pipe).toBeTruthy();
-  });
-});

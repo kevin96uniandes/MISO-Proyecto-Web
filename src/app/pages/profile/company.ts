588d2e73d938eb511b041fdeb28f691ee37e5ad6
Victor Castaneda <v.castanedap@uniandes.edu.co>
2024-11-22 15:15:58 -0500

    Solve user client information

---
+++ src/app/pages/profile/company.ts
@@ -0,0 +1,11 @@
+export interface Company {
+    id: number;
+    nombre_empresa: string;
+    numero_identificacion: string;
+    email: string;
+    sector: string;
+    telefono: string;
+    pais: string;
+    fecha_creacion: Date;
+    fecha_actualizacion: Date; 
+}
